a42a3ea26dbf96aa9ddee12fc4784b468218821a
Joe <aidevjoe@gmail.coma>
2023-06-05 15:19:47 +0800

    json code edit

--- lib/ui/modules/converters/json_yaml_converter_page.dart
+++ lib/ui/modules/converters/json_yaml_converter_page.dart
@@ -1,15 +1,38 @@
-import 'package:devbox/generated/locale_keys.g.dart';
-import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_code_editor/flutter_code_editor.dart';
+import 'package:highlight/languages/json.dart';
+import 'package:highlight/languages/yaml.dart';
+import 'package:flutter_highlight/themes/monokai-sublime.dart';
 
-class JsonYamlConverterPage extends StatelessWidget {
+class JsonYamlConverterPage extends StatefulWidget {
   const JsonYamlConverterPage({super.key});
 
   @override
+  State<JsonYamlConverterPage> createState() => _JsonYamlConverterPageState();
+}
+
+class _JsonYamlConverterPageState extends State<JsonYamlConverterPage> {
+  CodeController jsonController = CodeController(language: json);
+  CodeController yamlController = CodeController(language: yaml);
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.green,
-      child: Text(LocaleKeys.jsonYamlTitle.tr()),
-    );
+    return CodeTheme(
+        data: CodeThemeData(styles: monokaiSublimeTheme),
+        child: Row(children: [
+          Expanded(
+            child: CodeField(
+                gutterStyle: const GutterStyle(showErrors: true),
+                controller: jsonController,
+                expands: true),
+          ),
+          Expanded(
+            child: CodeField(
+              gutterStyle: const GutterStyle(showErrors: true),
+              controller: yamlController,
+              expands: true,
+            ),
+          )
+        ]));
   }
 }
